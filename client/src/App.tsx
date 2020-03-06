f7a583776a049ac1f85e5845d7036c2ccab4ad20
jcy6677 <jcy6677@gmail.com>
2020-03-06 13:02:28 +0900

    react bootstrap 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,18 +7,11 @@ import SigninPage from './pages/SigninPage';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div>
-      <HomePage/>
-      {/* <AdminPage/> */}
-    </div>
-=======
     <BrowserRouter>
       <Route exact path="/" component={HomePage}/>
       <Route path="/admin" component={AdminPage}/>
       <Route path="/test" component={SigninPage}/>
     </BrowserRouter>
->>>>>>> e4499d7ed5cd10b00dd492d317447314a6197e23
   );
 }
 
